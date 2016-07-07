865413a464e18c47de82c7cfb03851a071f459d7
Jongsoo Park <jongsoo.park@intel.com>
2016-07-07 11:16:36 -0700

    add platform info

--- test/europar16.sh
+++ test/europar16.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Tested on Intel(R) Xeon(R) CPU E5-2699 v4 @ 2.20GHz, 2 sockets, 11 cores per socket
 for i in `cat europar16.lst`; do for j in 11 22 44; do echo "OMP_NUM_THREADS=$j"; OMP_NUM_THREADS=$j ./trsv_test /scratch/jpark103/matrices/$i.mtx; done; done | tee europar16.log
